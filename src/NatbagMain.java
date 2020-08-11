7e70e74d094500ca9433e9fb4ee71dab1e4087fb
Ben-Shervi <Ben.shervi@gmail.com>
2020-08-11 19:34:45 +0300

    Update NatbagMain to match the html file

--- src/NatbagMain.java
+++ src/NatbagMain.java
@@ -17,7 +17,6 @@ public class NatbagMain {
 	public static void main(String[] args) {
 		Control control = new Control();
 		Program.hardCoding(control);
-		LocalDate today = LocalDate.now();
 		boolean isHtml = args.length > 0 && !args[0].equals("") ? args[0].equalsIgnoreCase("html") : false;
 		String nextLine = isHtml ? "<br>" : "\n";
 		boolean isDepartures = args.length > 1 && !args[1].equals("") ? Boolean.valueOf(args[1]) : true;
@@ -25,23 +24,16 @@ public class NatbagMain {
 		String country = args.length > 3 && !args[3].equals("") ? args[3] : null;
 		String city = args.length > 4 && !args[4].equals("") ? args[4] : null;
 		String airport = args.length > 5 && !args[5].equals("") ? args[5] : null;
-		int startDay = args.length > 6 && !args[6].equals("") ? Integer.valueOf(args[6]) : today.getDayOfMonth();
-		int startMonth = args.length > 7 && !args[7].equals("") ? Integer.valueOf(args[7]) : today.getMonthValue();
-		int startYear = args.length > 8 && !args[8].equals("") ? Integer.valueOf(args[8]) : today.getYear();
-		int endDay = args.length > 9 && !args[9].equals("") ? Integer.valueOf(args[9]) : 0;
-		int endMonth = args.length > 10 && !args[10].equals("") ? Integer.valueOf(args[10]) : 0;
-		int endYear = args.length > 11 && !args[11].equals("") ? Integer.valueOf(args[11]) : 0;
-		boolean sunday = args.length > 12 && !args[12].equals("") ? Boolean.valueOf(args[12]) : true;
-		boolean monday = args.length > 13 && !args[13].equals("") ? Boolean.valueOf(args[13]) : true;
-		boolean tuesday = args.length > 14 && !args[14].equals("") ? Boolean.valueOf(args[14]) : true;
-		boolean wednesday = args.length > 15 && !args[15].equals("") ? Boolean.valueOf(args[15]) : true;
-		boolean thursday = args.length > 16 && !args[16].equals("") ? Boolean.valueOf(args[16]) : true;
-		boolean friday = args.length > 17 && !args[17].equals("") ? Boolean.valueOf(args[17]) : true;
-		boolean saturday = args.length > 18 && !args[18].equals("") ? Boolean.valueOf(args[18]) : true;
+		LocalDate startDate = args.length > 6 && !args[6].equals("") ? LocalDate.parse(args[6]) : LocalDate.now();
+		LocalDate endDate = args.length > 7 && !args[7].equals("") ? LocalDate.parse(args[7]) : null;
+		boolean sunday = args.length > 8 && !args[8].equals("") ? Boolean.valueOf(args[8]) : true;
+		boolean monday = args.length > 9 && !args[9].equals("") ? Boolean.valueOf(args[9]) : true;
+		boolean tuesday = args.length > 10 && !args[10].equals("") ? Boolean.valueOf(args[10]) : true;
+		boolean wednesday = args.length > 11 && !args[11].equals("") ? Boolean.valueOf(args[11]) : true;
+		boolean thursday = args.length > 12 && !args[12].equals("") ? Boolean.valueOf(args[12]) : true;
+		boolean friday = args.length > 13 && !args[13].equals("") ? Boolean.valueOf(args[13]) : true;
+		boolean saturday = args.length > 14 && !args[14].equals("") ? Boolean.valueOf(args[14]) : true;
 		boolean[] days = { sunday, monday, tuesday, wednesday, thursday, friday, saturday };
-		LocalDate startDate = LocalDate.of(startYear, startMonth, startDay);
-		LocalDate endDate = (endYear != 0 || endMonth != 0 || endDay != 0) ? LocalDate.of(endYear, endMonth, endDay)
-				: null;
 		System.out.println(printFlights(
 				control.search(isDepartures, airline, country, city, airport, days, startDate, endDate), nextLine));
 	}
